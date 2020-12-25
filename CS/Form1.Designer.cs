939851bfd6d067e0784ba155b30b80b29dadbf6c
Alexey Zelenov (DevExpress) <alexey.zelenov@devexpress.com>
2020-12-25 15:38:59 +0300

    update v.11.2.5

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,79 @@
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid;
+
+namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myGridControl1 = new GridControl();
+            this.myGridView1 = new GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.myGridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myGridControl1
+            // 
+            this.myGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myGridControl1.Location = new System.Drawing.Point(0, 0);
+            this.myGridControl1.MainView = this.myGridView1;
+            this.myGridControl1.Name = "myGridControl1";
+            this.myGridControl1.Size = new System.Drawing.Size(731, 559);
+            this.myGridControl1.TabIndex = 0;
+            this.myGridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.myGridView1});
+            // 
+            // myGridView1
+            // 
+            this.myGridView1.GridControl = this.myGridControl1;
+            this.myGridView1.Name = "myGridView1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 559);
+            this.Controls.Add(this.myGridControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.myGridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GridControl myGridControl1;
+        private GridView myGridView1;
+
+    }
+}
+
